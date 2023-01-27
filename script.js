a82e4c7e8cf20ddab43af249a6bcf078955964cd
William <wiliancosta51@hotmail.com>
2023-01-27 19:56:37 -0300

    tela 3.2 finalizada

--- script.js
+++ script.js
@@ -1,75 +1,76 @@
 let perguntas;
 let verificarCor;
 let imagem;
+let imagemInput;
+let resposta;
+let incorreta1;
+let incorreta2;
+let incorreta3;
 function esconder() {
-    const tela1 = document.querySelector(".tela-1");
-    tela1.classList.add("escondido");
+  const tela1 = document.querySelector(".tela-1");
+  tela1.classList.add("escondido");
 
-    const tela31 = document.querySelector(".tela-31");
-    tela31.classList.remove("escondido");
+  const tela31 = document.querySelector(".tela-31");
+  tela31.classList.remove("escondido");
 }
 
 /*Até aqui Victor*/
 
 function checkInputs() {
-    let validation = 0;
-    const verificarImagem = document.querySelector(".imagem");
-    imagem = verificarImagem.value;
+  let validation = 0;
+  const verificarImagem = document.querySelector(".imagem");
+  imagem = verificarImagem.value;
 
-    const titulo = document.querySelector(".titulo input");
-    if (titulo.value.length < 20 || titulo.value.length > 65) {
-        alert("O número de caracteres digitados deve ser entre 20 e 65");
-    } else {
-        validation++;
-    }
+  const titulo = document.querySelector(".titulo input");
+  if (titulo.value.length < 20 || titulo.value.length > 65) {
+    alert("O número de caracteres digitados deve ser entre 20 e 65");
+  } else {
+    validation++;
+  }
 
-    if (checkurl(imagem) === false) {
+  if (checkurl(imagem) === false) {
+    alert("Insira um endereco valido.");
+  } else {
+    validation++;
+  }
+  /*Aqui estava a parte de verificar URL*/
+  const perguntas = document.querySelector(".perguntas input");
+  if (perguntas.value < 3) {
+    alert("A quantidade mínima de perguntas deve ser maior ou igual a 3!");
+  } else {
+    validation++;
+  }
 
-        alert("Insira um endereco valido.");
-    } else {
-        validation++;
-    }
-    /*Aqui estava a parte de verificar URL*/
-    const perguntas = document.querySelector(".perguntas input");
-    if (perguntas.value < 3) {
-        alert("A quantidade mínima de perguntas deve ser maior ou igual a 3!");
-    } else {
-        validation++;
-    }
+  const niveis = document.querySelector(".niveis input");
+  if (niveis.value < 2) {
+    alert("A quantidade mínima de níveis deve ser maior ou igual a 2!");
+  } else {
+    validation++;
+  }
 
-    const niveis = document.querySelector(".niveis input");
-    if (niveis.value < 2) {
-        alert("A quantidade mínima de níveis deve ser maior ou igual a 2!");
-    } else {
-        validation++;
-    }
-
-    if (validation === 4) {
-        console.log("oi");
-        aplicarTela3A();
-        const tela32 = document.querySelector(".tela-32");
-        tela32.classList.remove("escondido");
-        tela3A();
-    }
-    else {
-        alert("Deu erro, faça novamente");
-    }
+  if (validation === 4) {
+    console.log("oi");
+    aplicarTela3A();
+    const tela32 = document.querySelector(".tela-32");
+    tela32.classList.remove("escondido");
+    tela3A();
+  } else {
+    alert("Deu erro, faça novamente");
+  }
 }
 
-
 function aplicarTela3A() {
-    const esconder = document.querySelector(".tela-31");
-    esconder.classList.add("escondido");
+  const esconder = document.querySelector(".tela-31");
+  esconder.classList.add("escondido");
 }
 /*Até aqui Vitória*/
 
 function tela3A() {
+  const tela3A = document.querySelector(".tela-32");
 
-    const tela3A = document.querySelector(".tela-32");
-
-    tela3A.innerHTML = `
+  tela3A.innerHTML = `
   <h1 class="titulo-tela3">Crie suas perguntas</h1>
-    <div class="caixa-fake-1 escondido">
+    <div onclick="abrirPergunta1()" class="caixa-fake-1 escondido">
       <h1>Pergunta 1</h1>
       <img src="./imagens/Vector.jpg" />
     </div>
@@ -82,50 +83,50 @@ function tela3A() {
       </div>
       <div class="resposta-pergunta1">
         <h1>Resposta Correta</h1>
-        <input type="text" value="" placeholder="Resposta correta" />
+        <input type="text" value="" class="reposta-correta" placeholder="Resposta correta" />
         <input class="imagem" type="text" value="" placeholder="URL da imagem" />
       </div>
       <div class="resposta-incorreta">
         <h1>Respostas incorretas</h1>
         <input
-          class="input1"
           type="text"
           value=""
+          class="input1"
           placeholder="Resposta incorreta 1"
         />
         <input
-          class="input1"
           type="text"
           value=""
+          class="input3"
           placeholder="URL da imagem 1"
         />
         <input
-          class="input2"
           type="text"
-          value=""
+          value="" 
+          class="input2"
           placeholder="Resposta incorreta 2"
         />
         <input
-          class="input3"
           type="text"
           value=""
+          class="input3"
           placeholder="URL da imagem 2"
         />
         <input
-          class="input4"
           type="text"
           value=""
+          class="input4"
           placeholder="Resposta incorreta 3"
         />
         <input
-          class="input5"
           type="text"
           value=""
+          class="input3"
           placeholder="URL da imagem 3"
         />
       </div>
     </div>
-    <div class="caixa-fake-2">
+    <div onclick= "abrirPergunta2(this)" class="caixa-fake-2">
       <h1>Pergunta 2</h1>
       <img src="./imagens/Vector.jpg" />
     </div>
@@ -138,54 +139,54 @@ function tela3A() {
       </div>
       <div class="resposta-pergunta1">
         <h1>Resposta Correta</h1>
-        <input type="text" value="" placeholder="Resposta correta" />
+        <input  type="text" value="" class="reposta-correta" placeholder="Resposta correta" />
         <input class="imagem" type="text" value="" placeholder="URL da imagem" />
       </div>
       <div class="resposta-incorreta">
         <h1>Respostas incorretas</h1>
         <input
-          class="input1"
+          class=" input1"
           type="text"
           value=""
           placeholder="Resposta incorreta 1"
         />
         <input
-          class="input1"
+          class="input3 imagem"
           type="text"
           value=""
           placeholder="URL da imagem 1"
         />
         <input
-          class="input2"
+          class=" input1"
           type="text"
           value=""
           placeholder="Resposta incorreta 2"
         />
         <input
-          class="input3"
+          class="input3 imagem"
           type="text"
           value=""
           placeholder="URL da imagem 2"
         />
         <input
-          class="input4"
+          class="input1 imagem"
           type="text"
           value=""
           placeholder="Resposta incorreta 3"
         />
         <input
-          class="input5"
+          class="input3 imagem"
           type="text"
           value=""
           placeholder="URL da imagem 3"
         />
       </div>
     </div>
-    <div class="caixa-fake-3">
+    <div onclick= "abrirPergunta3(this)" class="caixa-fake-3">
       <h1>Pergunta 3</h1>
       <img src="./imagens/Vector.jpg" />
     </div>
-    <div class="caixa-de-perguntas-3 caixa-tela-3 escondido">
+    <div  class="caixa-de-perguntas-3 caixa-tela-3 escondido">
       <div class="pergunta1">
         <br />
         <h1>Pergunta 3</h1>
@@ -194,7 +195,7 @@ function tela3A() {
       </div>
       <div class="resposta-pergunta1">
         <h1>Resposta Correta</h1>
-        <input type="text" value="" placeholder="Resposta correta" />
+        <input type="text" value="" class="reposta-correta" placeholder="Resposta correta" />
         <input class="imagem" type="text" value="" placeholder="URL da imagem" />
       </div>
       <div class="resposta-incorreta">
@@ -206,31 +207,31 @@ function tela3A() {
           placeholder="Resposta incorreta 1"
         />
         <input
-          class="input1"
+          class="input3 imagem"
           type="text"
           value=""
           placeholder="URL da imagem 1"
         />
         <input
-          class="input2"
+          class=" input1"
           type="text"
           value=""
           placeholder="Resposta incorreta 2"
         />
         <input
-          class="input3"
+          class="input3 imagem"
           type="text"
           value=""
           placeholder="URL da imagem 2"
         />
         <input
-          class="input4"
+          class=" input1"
           type="text"
           value=""
           placeholder="Resposta incorreta 3"
         />
         <input
-          class="input5"
+          class="input3 imagem"
           type="text"
           value=""
           placeholder="URL da imagem 3"
@@ -239,42 +240,153 @@ function tela3A() {
     </div>
     <div onclick="verificarPergunta()" class="botao-quizz">Prosseguir pra criar níveis</div>
     `;
-
-
 }
 function verificarPergunta() {
+  perguntas = document.querySelector(".input-pergunta");
+  const cor = document.querySelector(".input-cor");
+  verificarCor = cor.value;
+  verificarImagem = document.querySelector(".imagem");
+  verificarImagemInput = document.querySelector(".input3");
 
-    perguntas = document.querySelector(".input-pergunta");
-    const cor = document.querySelector(".input-cor");
-    verificarCor = cor.value;
-    verificarImagem = document.querySelector(".imagem");
-
-    imagem = verificarImagem.value;
-    const verificarPergunta = perguntas.value;
-    console.log(verificarPergunta);
-    if (verificarPergunta.length <= "19") {
-        alert("Insira no mínimo 20 caracteres em uma pergunta.");
-    }
-    is_hexadecimal(cor.value);
-    checkurl(verificarImagem.value);
+  imagem = verificarImagem.value;
+  imagemInput = verificarImagemInput.value;
+  const verificarPergunta = perguntas.value;
+  if (verificarPergunta.length <= "19") {
+    alert("Insira no mínimo 20 caracteres em uma pergunta.");
+  }
+  if (imagemInput === "") {
+    alert("Insira a URL de uma Imagem!");
+  }
+  is_hexadecimal(cor.value);
+  checkurl(verificarImagem.value);
+  checkurl(verificarImagemInput.value);
+  verificarResposta();
+  respostaCorretaIncorreta();
+  /*escondeTela32();*/ // <== aqui tem que tirar o comentário quando arrumar o código da vic
 }
-function is_hexadecimal(verificarCor) {
-    let regex = new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+/*function escondeTela32() {
+  const tela32 = document.querySelector(".tela-32");
+  tela32.classList.add("escondido");
 
-    if (regex.test(verificarCor)) {
-        return true;
-    } else {
-        alert("Insira uma cor hexadecimal!");
-    }
+  const tela33 = document.querySelector(".tela-33");
+
+  tela33.classList.remove("escondido");
+}*/
+
+//Aqui tem que mudar o querySelector pro nome da div da Vic
+function is_hexadecimal(verificarCor) {
+  let regex = new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+
+  if (regex.test(verificarCor)) {
+    return true;
+  } else {
+    alert("Insira uma cor hexadecimal!");
+  }
 }
 function checkurl(imagem) {
-    regexp =
-        /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-    if (regexp.test(imagem)) {
-        return true;
-    } else {
-        alert("Insira um endereco valido.");
-        return false;
-    }
+  regexp =
+    /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(imagem)) {
+    return true;
+  } else {
+    alert("Insira um endereco valido.");
+    return false;
+  }
+}
+function checkurl(imagemInput) {
+  regexp =
+    /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(imagemInput)) {
+    return true;
+  } else {
+    alert("Insira um endereço válido.");
+    return false;
+  }
+}
+function verificarResposta() {
+  const pergunta = document.querySelector(".reposta-correta");
+  const input1 = document.querySelector(".resposta-incorreta .input1");
+
+  const input2 = document.querySelector(".resposta-incorreta .input2");
+  const input4 = document.querySelector(".resposta-incorreta .input4");
+
+  resposta = pergunta.value;
+  incorreta1 = input1.value;
+  incorreta2 = input2.value;
+  incorreta4 = input4.value;
+}
+function respostaCorretaIncorreta() {
+  if (resposta === "") {
+    alert("Insira uma resposta correta!");
+  } else if (incorreta1 === "" && incorreta2 === "" && incorreta4 === "") {
+    alert("Insira no mínimo 1 resposta incorreta");
+  }
+}
+function abrirPergunta1() {
+  const esconderFake1 = document.querySelector(".caixa-fake-1");
+
+  esconderFake1.classList.add("escondido");
+
+  const mostrarPergunta1 = document.querySelector(".caixa-tela-3");
+
+  mostrarPergunta1.classList.remove("escondido");
+
+  const esconderPergunta2 = document.querySelector(".caixa-de-perguntas-2");
+
+  esconderPergunta2.classList.add("escondido");
+
+  const mostrarPergunta3 = document.querySelector(".caixa-de-perguntas-3");
+
+  mostrarPergunta3.classList.add("escondido");
+
+  const mostrarFake3 = document.querySelector(".caixa-fake-3");
+
+  mostrarFake3.classList.remove("escondido");
+
+  const mostrarFake2 = document.querySelector(".caixa-fake-2");
+
+  mostrarFake2.classList.remove("escondido");
+}
+function abrirPergunta2() {
+  const esconderPergunta1 = document.querySelector(".caixa-tela-3");
+
+  esconderPergunta1.classList.add("escondido");
+
+  const perguntaFake1 = document.querySelector(".caixa-fake-1");
+
+  perguntaFake1.classList.remove("escondido");
+
+  const mostrarPergunta2 = document.querySelector(".caixa-de-perguntas-2");
+
+  mostrarPergunta2.classList.remove("escondido");
+
+  const perguntaFake2 = document.querySelector(".caixa-fake-2");
+
+  perguntaFake2.classList.add("escondido");
+
+  const esconderFake3 = document.querySelector(".caixa-fake-3");
+
+  esconderFake3.classList.remove("escondido");
+
+  const mostrarPergunta3 = document.querySelector(".caixa-de-perguntas-3");
+
+  mostrarPergunta3.classList.add("escondido");
+}
+function abrirPergunta3() {
+  const esconderPergunta2 = document.querySelector(".caixa-de-perguntas-2");
+
+  esconderPergunta2.classList.add("escondido");
+
+  const mostrarFake2 = document.querySelector(".caixa-fake-2");
+
+  mostrarFake2.classList.remove("escondido");
+
+  const mostrarPergunta3 = document.querySelector(".caixa-de-perguntas-3");
+
+  mostrarPergunta3.classList.remove("escondido");
+
+  const esconderFake3 = document.querySelector(".caixa-fake-3");
+
+  esconderFake3.classList.add("escondido");
 }
 /*Até aqui é o do WIlliam*/
